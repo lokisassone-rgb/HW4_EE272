2f7752d9aa3dd24c960b24d582b6cfecac4349e6
Bahaa Lawrence <bahaalawrence01@gmail.com>
2026-02-03 20:53:24 -0800

    Weight Double Buffer

--- src/WeightDoubleBuffer.h
+++ src/WeightDoubleBuffer.h
@@ -14,6 +14,33 @@ public:
     {
         // -------------------------------
         // Your code starts here
+        Params params = paramsIn.read();
+        
+        for (int oy1 = 0; oy1 < params.OY1; oy1++){
+            for (int ox1 = 0; ox1 < params.OX1; ox1++){
+                for (int oc1 = 0; oc1 < params.OC1; oc1++){
+                    chanStruct<PackedInt<WEIGHT_PRECISION, OC0>, size> temp;
+                    for (int ic1 = 0; ic1 < params.IC1; ic1++){
+                        for (int fy = 0; fy < params.FY; fy++){
+                            for (int fx = 0; fx < params.FX; fx++){
+                                int buffer_add = ic1 * (params.FY * params.FX) + fy * params.FX + fx; //calculate address linearly
+                                PackedInt<WEIGHT_PRECISION, 4> tempdinread = din.read();
+                                for (int oc0_block = 0; oc0_block < OC0/4; oc0_block++){
+                                    PackedInt<WEIGHT_PRECISION, OC0> tempdinwrite;
+                                    tempdinwrite.value[oc0_block*4] = tempdinread.value[0];
+                                    tempdinwrite.value[oc0_block*4+1] = tempdinread.value[1];
+                                    tempdinwrite.value[oc0_block*4+2] = tempdinread.value[2];
+                                    tempdinwrite.value[oc0_block*4+3] = tempdinread.value[3];
+                                    temp.data[buffer_add * (OC0/4) + oc0_block] = tempdinwrite;
+                                }
+                            }
+                        }
+                    }
+                }
+                dout.write(temp);
+            }
+        }
+
 
         // Your code ends here
         // -------------------------------
@@ -32,6 +59,25 @@ public:
     {
         // -------------------------------
         // Your code starts here
+        Params params = paramsIn.read();
+
+        for (int oy1 = 0; oy1 < params.OY1; oy1++){
+            for (int ox1 = 0; ox1 < params.OX1; ox1++){
+                chanStruct<PackedInt<WEIGHT_PRECISION, OC0>, size> temp = din.read(); //create new tile 
+                for (int oc1 = 0; oc1 < params.OC1; oc1++){
+                    for (int ic1 = 0; ic1 < params.IC1; ic1++){
+                        for (int fy = 0; fy < params.FY; fy++){
+                            for (int fx = 0; fx < params.FX; fx++){
+                                int buffer_add = ic1 * (params.FY * params.FX) + fy * params.FX + fx; //calculate address linearly
+                                for (int oc0_block = 0; oc0_block < OC0/4; oc0_block++){
+                                    dout.write( temp.data[buffer_add * (OC0/4) + oc0_block] );
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         // Your code ends here
         // -------------------------------
