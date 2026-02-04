6c8106ed07b3bbe9f13d53f4bcab1f8b74e74712
Bahaa Lawrence <bahaalawrence01@gmail.com>
2026-02-03 21:40:54 -0800

    Code Update

--- src/WeightDoubleBuffer.h
+++ src/WeightDoubleBuffer.h
@@ -15,7 +15,7 @@ public:
         // -------------------------------
         // Your code starts here
         Params params = paramsIn.read();
-        std::cout << "[WeightDoubleBufferWriter] Params: OY1=" << params.OY1 << ", OX1=" << params.OX1 << ", OC1=" << params.OC1 << ", IC1=" << params.IC1 << ", FX=" << params.FX << ", FY=" << params.FY << std::endl;
+        printf("[WeightDoubleBufferWriter] Params: OY1=%d, OX1=%d, OC1=%d, IC1=%d, FX=%d, FY=%d\n", (int)params.OY1, (int)params.OX1, (int)params.OC1, (int)params.IC1, (int)params.FX, (int)params.FY);
 
         for (uint_16 oy1 = 0; oy1 < params.OY1; oy1++){
             for (uint_16 ox1 = 0; ox1 < params.OX1; ox1++){
@@ -28,16 +28,17 @@ public:
                             for (uint_16 fx = 0; fx < params.FX; fx++){
                                 for (uint_16 weight_index = 0; weight_index < OC0/4; weight_index++){
                                     input = din.read();
-                                    std::cout << "[WeightDoubleBufferWriter] Read input: ic=" << ic << ", fy=" << fy << ", fx=" << fx << ", weight_index=" << weight_index << " input.value=[";
+                                    printf("[WeightDoubleBufferWriter] Read input: ic=%d, fy=%d, fx=%d, weight_index=%d input.value=[%d,%d,%d,%d]\n",
+                                        (int)ic, (int)fy, (int)fx, (int)weight_index,
+                                        (int)input.value[0], (int)input.value[1], (int)input.value[2], (int)input.value[3]);
                                     for (uint_16 j = 0; j < 4; j++) {
-                                        std::cout << input.value[j] << (j < 3 ? "," : "]\n");
                                         tile.data[ic*(params.FY)*(params.FX) + fy*(params.FX) + fx].value[weight_index*4+j] = input.value[j];
                                     }
                                 }
                             }
                         }
                     }
-                    std::cout << "[WeightDoubleBufferWriter] Writing tile for oy1=" << oy1 << ", ox1=" << ox1 << ", oc1=" << oc1 << std::endl;
+                    printf("[WeightDoubleBufferWriter] Writing tile for oy1=%d, ox1=%d, oc1=%d\n", (int)oy1, (int)ox1, (int)oc1);
                     dout.write(tile);
                 }
             }
@@ -60,19 +61,19 @@ public:
         // -------------------------------
         // Your code starts here
         Params params = paramsIn.read();
-        std::cout << "[WeightDoubleBufferReader] Params: OY1=" << params.OY1 << ", OX1=" << params.OX1 << ", OC1=" << params.OC1 << ", IC1=" << params.IC1 << ", FX=" << params.FX << ", FY=" << params.FY << std::endl;
+        printf("[WeightDoubleBufferReader] Params: OY1=%d, OX1=%d, OC1=%d, IC1=%d, FX=%d, FY=%d\n", (int)params.OY1, (int)params.OX1, (int)params.OC1, (int)params.IC1, (int)params.FX, (int)params.FY);
 
         for (uint_16 oy1 = 0; oy1 < params.OY1; oy1++){
             for (uint_16 ox1 = 0; ox1 < params.OX1; ox1++){
                 for (uint_16 oc1 = 0; oc1 < params.OC1; oc1++){
                     chanStruct<PackedInt<WEIGHT_PRECISION, OC0>,size> tmp = din.read();
-                    std::cout << "[WeightDoubleBufferReader] Read tile for oy1=" << oy1 << ", ox1=" << ox1 << ", oc1=" << oc1 << std::endl;
+                    printf("[WeightDoubleBufferReader] Read tile for oy1=%d, ox1=%d, oc1=%d\n", (int)oy1, (int)ox1, (int)oc1);
                     PackedInt<WEIGHT_PRECISION, OC0> weights_going_to_systolic_array;
                     for (uint_16 ic = 0; ic < (params.IC1*IC0); ic++){
                         for (uint_16 fy = 0; fy < params.FY; fy++){
                             for (uint_16 fx = 0; fx < params.FX; fx++){
                                 weights_going_to_systolic_array = tmp.data[ic*(params.FY)*(params.FX) + fy*(params.FX) + fx];
-                                std::cout << "[WeightDoubleBufferReader] Writing weights: ic=" << ic << ", fy=" << fy << ", fx=" << fx << std::endl;
+                                printf("[WeightDoubleBufferReader] Writing weights: ic=%d, fy=%d, fx=%d\n", (int)ic, (int)fy, (int)fx);
                                 dout.write(weights_going_to_systolic_array);
                             }
                         }
@@ -96,7 +97,7 @@ public:
                       ac_channel<Params> &paramsIn)
     {
         Params params = paramsIn.read();
-        std::cout << "[WeightDoubleBuffer] Starting run with Params: OY1=" << params.OY1 << ", OX1=" << params.OX1 << ", OC1=" << params.OC1 << ", IC1=" << params.IC1 << ", FX=" << params.FX << ", FY=" << params.FY << std::endl;
+        printf("[WeightDoubleBuffer] Starting run with Params: OY1=%d, OX1=%d, OC1=%d, IC1=%d, FX=%d, FY=%d\n", (int)params.OY1, (int)params.OX1, (int)params.OC1, (int)params.IC1, (int)params.FX, (int)params.FY);
 
         // #ifndef __SYNTHESIS__
         // ac_int<ac::log2_ceil<size>::val, false> block_size = IC0*params.IC1*params.FX*params.FY;
@@ -106,11 +107,12 @@ public:
         weightDoubleBufferReaderParams.write(params);
         weightDoubleBufferWriterParams.write(params);
 
-        std::cout << "[WeightDoubleBuffer] Calling Writer..." << std::endl;
+        printf("[WeightDoubleBuffer] Calling Writer...\n");
         weightDoubleBufferWriter.run(weightDoubleBufferWriterParams, weights_in, mem);
-        std::cout << "[WeightDoubleBuffer] Calling Reader..." << std::endl;
+        printf("[WeightDoubleBuffer] Calling Reader...\n");
         weightDoubleBufferReader.run(weightDoubleBufferReaderParams, mem, weights_out);
-        std::cout << "[WeightDoubleBuffer] Finished run." << std::endl;
+        printf("[WeightDoubleBuffer] Finished run.\n");
+    #include <cstdio>
     }
 
 private:
