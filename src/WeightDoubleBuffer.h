af609853ffc944786b0a41924e028e8f6ede58d6
Bahaa Lawrence <bahaalawrence01@gmail.com>
2026-02-03 22:33:09 -0800

    Git Branch

--- src/WeightDoubleBuffer.h
+++ src/WeightDoubleBuffer.h
@@ -14,34 +14,35 @@ public:
     {
         // -------------------------------
         // Your code starts here
-        printf("size = %d\n", size);
         Params params = paramsIn.read();
 
-        for (int oy1 = 0; oy1 < params.OY1; oy1++){
-            for (int ox1 = 0; ox1 < params.OX1; ox1++){
-                for (int oc1 = 0; oc1 < params.OC1; oc1++){
+        for (uint_16 oy1 = 0; oy1 < params.OY1; oy1++){
+            for (uint_16 ox1 = 0; ox1 < params.OX1; ox1++){
+                for (uint_16 oc1 = 0; oc1 < params.OC1; oc1++){
                     chanStruct<PackedInt<WEIGHT_PRECISION, OC0>, size> tile;
                     PackedInt<WEIGHT_PRECISION, 4> input;
-                
                     //define size of a weight tile (IC1*IC0*FX*FY)
-                    int numberofWeightsPerTile = int(params.IC1) * IC0 * int(params.FX) * int(params.FY);
-                    for (int i = 0; i < numberofWeightsPerTile; i++) {
-                        for (int weight_index = 0; weight_index < OC0/4; weight_index++){
-                            input = din.read();
-                            for (int j = 0; j < 4; j++) {
-                                tile.data[i].value[weight_index*4+j] = input.value[j];
+                    for (uint_16 ic = 0; ic < (params.IC1*IC0); ic++){
+                        for (uint_16 fy = 0; fy < params.FY; fy++){
+                            for (uint_16 fx = 0; fx < params.FX; fx++){
+                                for (uint_16 weight_index = 0; weight_index < OC0/4; weight_index++){
+                                    input = din.read();
+                                    for (uint_16 j = 0; j < 4; j++){
+                                        tile.data[ic*(params.FY)*(params.FX) + fy*(params.FX) + fx].value[weight_index*4+j] = input.value[j];
                                     }
                                 }
+                  
                             }
-                            dout.write(tile);
                         }
                     }
-
+                    dout.write(tile);
                 }
             }
-        };
+        }
         // Your code ends here
         // -------------------------------
+    }
+};
 
 template <int size, int IC0, int OC0>
 class WeightDoubleBufferReader{
@@ -57,22 +58,28 @@ public:
         // Your code starts here
         Params params = paramsIn.read();
 
-        for (int oy1 = 0; oy1 < params.OY1; oy1++){
-            for (int ox1 = 0; ox1 < params.OX1; ox1++){
-                for (int oc1 = 0; oc1 < params.OC1; oc1++){
-                    
+        for (uint_16 oy1 = 0; oy1 < params.OY1; oy1++){
+            for (uint_16 ox1 = 0; ox1 < params.OX1; ox1++){
+                for (uint_16 oc1 = 0; oc1 < params.OC1; oc1++){
                     chanStruct<PackedInt<WEIGHT_PRECISION, OC0>,size> tmp = din.read();
-                    int numberofWeightsPerTile = int(params.IC1) * IC0 * int(params.FX) * int(params.FY);
-                    for (int i = 0; i < numberofWeightsPerTile; i++){
-                        dout.write(tmp.data[i]);
+                    PackedInt<WEIGHT_PRECISION, OC0> weights_going_to_systolic_array;
+                    for (uint_16 ic = 0; ic < (params.IC1*IC0); ic++){
+                        for (uint_16 fy = 0; fy < params.FY; fy++){
+                            for (uint_16 fx = 0; fx < params.FX; fx++){
+                                weights_going_to_systolic_array = tmp.data[ic*(params.FY)*(params.FX) + fy*(params.FX) + fx];
+                                dout.write(weights_going_to_systolic_array);
                             }
                         }
                     }
                 }
             }
-        };
+        }
+
+
         // Your code ends here
         // -------------------------------
+    }
+};
 
 template <int size, int IC0, int OC0>
 class WeightDoubleBuffer{
@@ -96,7 +103,6 @@ public:
 
         weightDoubleBufferWriter.run(weightDoubleBufferWriterParams, weights_in, mem);
         weightDoubleBufferReader.run(weightDoubleBufferReaderParams, mem, weights_out);
-    #include <cstdio>
     }
 
 private:
