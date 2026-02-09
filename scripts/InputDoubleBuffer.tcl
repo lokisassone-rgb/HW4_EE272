0c9c0f987f6d19354dbc3b8cf7f1f5c4ae60a846
Bahaa Lawrence <bahaalawrence01@gmail.com>
2026-02-08 21:24:18 -0800

    Update code

--- scripts/InputDoubleBuffer.tcl
+++ scripts/InputDoubleBuffer.tcl
@@ -20,7 +20,7 @@ go assembly
 # Your code starts here
 set word_width [expr ${ARRAY_DIMENSION} * 8]
 
-directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/mem:cns -STAGE_REPLICATION 2
+directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/mem:cns -STAGE_REPLICATION 4
 
 directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/InputDoubleBufferReader<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/din -WORD_WIDTH $word_width
 directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/InputDoubleBufferWriter<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/dout -WORD_WIDTH $word_width
--- scripts/WeightDoubleBuffer.tcl
+++ scripts/WeightDoubleBuffer.tcl
@@ -19,7 +19,7 @@ go assembly
 # Set the correct word widths and the stage replication
 # Your code starts here
 set word_width [expr ${ARRAY_DIMENSION} * 8]
-directive set /WeightDoubleBuffer<8192,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/mem:cns -STAGE_REPLICATION 2
+directive set /WeightDoubleBuffer<8192,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/mem:cns -STAGE_REPLICATION 4
 
 directive set /WeightDoubleBuffer<8192,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/WeightDoubleBufferReader<8192,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/din -WORD_WIDTH $word_width
 directive set /WeightDoubleBuffer<8192,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/WeightDoubleBufferWriter<8192,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/dout -WORD_WIDTH $word_width
