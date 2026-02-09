2d2f7616a2afd9d7ae24750a1a2f213fe54431e7
Bahaa Lawrence <bahaalawrence01@gmail.com>
2026-02-08 16:44:20 -0800

    Added temp width

--- scripts/WeightDoubleBuffer.tcl
+++ scripts/WeightDoubleBuffer.tcl
@@ -24,6 +24,7 @@ directive set /WeightDoubleBuffer<8192,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/me
 directive set /WeightDoubleBuffer<8192,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/din -WORD_WIDTH $word_width
 directive set /WeightDoubleBuffer<8192,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/dout -WORD_WIDTH $word_width
 directive set /WeightDoubleBuffer<8192,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/mem -WORD_WIDTH $word_width
+directive set /WeightDoubleBuffer<8192,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/.../temp.data.value -WORD_WIDTH $word_width
 # Your code ends here
 # -------------------------------
 
