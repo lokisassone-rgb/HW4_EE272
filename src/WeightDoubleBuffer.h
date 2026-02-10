f0965a3aca7018065b34a9fa1ab6e4e98d529c2b
lokisassone-rgb <lokisassone@gmail.com>
2026-02-10 11:44:03 -0800

    updated weight double buffer

--- src/WeightDoubleBuffer.h
+++ src/WeightDoubleBuffer.h
@@ -32,10 +32,9 @@ public:
                     int numTileItems = int(params.IC1) * int(params.FY) * int(params.FX) * IC0;
                     for (int tileItemIdx = 0; tileItemIdx < size; tileItemIdx++) {
                         if (tileItemIdx >= numTileItems) { break; }
-                        #pragma hls_unroll yes
+                        #pragma hls_pipeline_init_interval 1
                         for (int inputItemIdx = 0; inputItemIdx < OC0/4; inputItemIdx++) {
                             inputItem = din.read();
-                            #pragma hls_unroll yes
                             for (int i = 0; i < 4; i++) {
                                 temp.data[tileItemIdx].value[4*inputItemIdx + i] = inputItem.value[i]; //
                             }
