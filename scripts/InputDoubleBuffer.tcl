964cddcc4e9405903e1f8e8c02e2fdea58ac2418
Bahaa Lawrence <bahaalawrence01@gmail.com>
2026-02-09 18:04:08 -0800

    code update

--- scripts/InputDoubleBuffer.tcl
+++ scripts/InputDoubleBuffer.tcl
@@ -22,11 +22,11 @@ set word_width [expr ${ARRAY_DIMENSION} * 8]
 set block_size ${ARRAY_DIMENSION}
 
 directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/mem:cns -STAGE_REPLICATION 2
-directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/inputDoubleBufferWriter/tmp -BLOCK_SIZE $block_size
-directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/inputDoubleBufferReader/tmp -BLOCK_SIZE $block_size
+directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/InputDoubleBufferWriter/tmp -BLOCK_SIZE $block_size
+directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/InputDoubleBufferReader/tmp -BLOCK_SIZE $block_size
 
-directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/inputDoubleBufferReader/din -WORD_WIDTH $word_width
-directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/inputDoubleBufferWriter/dout -WORD_WIDTH $word_width
+directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/InputDoubleBufferReader/din -WORD_WIDTH $word_width
+directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/InputDoubleBufferWriter/dout -WORD_WIDTH $word_width
 directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/mem -WORD_WIDTH $word_width
 directive set /InputDoubleBuffer<4096,${ARRAY_DIMENSION},${ARRAY_DIMENSION}>/.../temp.data.value -match glob -WORD_WIDTH $word_width
 
